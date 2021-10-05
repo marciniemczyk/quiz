2c59747ade0879f9266189f20dbb692a81fe1a22
Marcin <mail.niemczyk@gmail.com>
2021-10-05 18:27:19 +0200

    Add quiz questions

--- src/main/java/com/myproject/quiz/services/QuizDataService.java
+++ src/main/java/com/myproject/quiz/services/QuizDataService.java
@@ -1,9 +1,13 @@
 package com.myproject.quiz.services;
 
 import com.myproject.quiz.dto.CategoriesDto;
+import com.myproject.quiz.dto.QuestionsDto;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Service
 @Log
@@ -13,5 +17,19 @@ public class QuizDataService {
         RestTemplate restTemplate = new RestTemplate();
         CategoriesDto result = restTemplate.getForObject("https://opentdb.com/api_category.php", CategoriesDto.class);
         log.info("Quiz categories: " + result.getCategories());
+    }
+
+    public void getQuizQuestions() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        URI uri = UriComponentsBuilder.fromHttpUrl("https://opentdb.com/api.php")
+                .queryParam("amount", 2)
+                .queryParam("category", 25)
+                .queryParam("difficulty", "medium")
+                .build().toUri();
+        log.info("Quiz question retrieve URL: " + uri);
+
+        QuestionsDto result = restTemplate.getForObject(uri, QuestionsDto.class);
+        log.info("Quiz questions: " + result.getResults());
     }
 }
